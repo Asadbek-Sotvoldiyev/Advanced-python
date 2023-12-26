0ad0e9ad6cdbbf50a2e500abb04cc65c0d5d0fb6
Asadbek Sotvoldiyev <blogasadbek@gmail.com>
2023-12-26 13:57:12 +0500

    My decorator example 2 - Timing

---
+++ Timing.py
@@ -0,0 +1,22 @@
+#Practical example 2 - Timing
+import time
+
+def timed(function):
+    def wrapper(*args, **kwargs):
+        before = time.time()
+        value = function(*args, **kwargs)
+        after = time.time()
+        fname = function.__name__
+        print(f"{fname} funksiyasining bajarilish vaqti: {after-before}")
+        return value
+    return wrapper
+
+@timed
+def myfunction(x):
+    result = 1
+    for i in range(1,x):
+        result*=i
+    return result
+
+myfunction(90000)
+#result: 3.8576242923736572
