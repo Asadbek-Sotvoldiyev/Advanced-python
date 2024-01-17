3a7ef6bdd3816b3fcfd35c88b39e1386e0c36844
Asadbek Sotvoldiyev <blogasadbek@gmail.com>
2024-01-17 15:19:29 +0500

    linked list in Algorithm

--- Algorithms/linked_list.py
+++ Algorithms/linked_list.py
@@ -17,9 +17,34 @@ class LinkedList:
         while current.next!=None:
             current = current.next
             print(current.data)
+    def len(self):
+        count = 0
+        current = self.head
+        while current.next!=None:
+            current = current.next
+            count+=1
+        return count
+    def remove(self, key):
+        temp = self.head
+
+        if temp is not None:
+            self.head = temp.next
+            temp = None
+            return
+        while temp is not None:
+            if temp.data==key:
+                break
+            prev = temp
+            temp = temp.next
+        if temp==None:
+            return
+        prev.next = temp.next
+        temp = None
+
 
 my_list = LinkedList()
 my_list.push('Asadbek')
 my_list.push('Yahyobek')
 my_list.push('Azizbek')
+my_list.remove('Asadbek')
 my_list.show()
