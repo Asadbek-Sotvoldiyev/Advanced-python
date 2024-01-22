54ccfd1c4f19058e7948d73f8e47c6b2e0f9b8e1
Asadbek Sotvoldiyev <blogasadbek@gmail.com>
2024-01-22 08:46:45 +0500

    My iterator

---
+++ Mixed codes/my_iterator.py
@@ -0,0 +1,18 @@
+class MyIterator:
+    def __init__(self, data):
+        self.data = data
+        self.index = 0
+        
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.index<len(self.data):
+            result = self.data[self.index]
+            self.index+=1
+            return result
+        else:
+            raise StopIteration
+        
+my_list = [1,2,3,4,5]
+data = MyIterator(my_list)
